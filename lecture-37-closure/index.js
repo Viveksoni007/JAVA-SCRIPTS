4e151faa8e06c18504bfe49b5d9dc866a947bc8d
Vivek Soni <consolegamingplay@gmail.com>
2026-09-22 18:46:45 +0530

    closure

--- lecture-37-closure/index.js
+++ lecture-37-closure/index.js
@@ -1,1 +1,40 @@
-console.log("hoo");
+
+// function outter(){
+//     let a=5;
+//     function inner(){
+//         console.log(a);
+//     }
+    
+//     return inner;
+// }
+// const response =outter();
+// response();
+// console.log(outter);
+// console.log(response);
+
+
+
+function outter() {
+    let count = 0;
+    function counter() {
+        count = count + 1;
+        console.log(count);
+    }
+
+    return counter
+}
+
+const counter1 = outter();
+const counter2 = outter();
+counter1();
+counter1();
+counter1();
+counter1();
+counter1();
+
+
+counter2();
+counter2();
+counter2();
+counter2();
+counter2();
