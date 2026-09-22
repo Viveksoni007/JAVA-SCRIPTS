bcfc4112efe2faa63ae415786c28a69486cddd21
Vivek Soni <consolegamingplay@gmail.com>
2026-09-22 15:18:42 +0530

    Fixed folder issue and uploaded code

---
+++ lecture-36_executions andContext/index.js
@@ -0,0 +1,61 @@
+
+// console.log(a);
+// var a=40;
+
+// console.log(b);
+// let b=10;
+// 
+// outer();
+// function outer(){
+//     console.log("hii");
+// }
+
+// fun2();
+// var fun2 =function(){
+//     console.log("hii");
+// }
+
+// fun2();
+// const fun2 =function(){
+//     console.log("hii");
+// }
+
+// fun3();
+// const fun3=()=>{
+//     console.log("hellooo");
+// }
+
+// var a=10;
+// let b=5;
+// console.log(a+b);
+
+// function outer(){
+//     let num1=10;
+//     let num2=20;
+
+//     function inner(){
+//         let num1=50;
+//         let num2=50;
+//         return num1+num2;
+//     }
+//     const result=inner()+num1+num2;
+//     return result;
+
+
+// }
+// const result=outer();
+// console.log(result);
+
+
+
+// function recurse(){
+//     recurse();
+// }
+
+
+// let a=10;
+// function gun(){
+//     console.log(a);
+//     // let a=6;
+// }
+// gun();
